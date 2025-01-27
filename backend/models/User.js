11be7d81ddab88ec7efeaa6ca2560ac44d62ca32
Balasubramanian Palani <85837974+Balasubramanian24@users.noreply.github.com>
2025-01-27 16:43:54 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -14,7 +14,7 @@ const UserSchema = new mongoose.Schema({
 });
 
 // Hash the password before saving
-UserSchema.pre('save', async function (next) {
+UserSchema.pre('save', async function ( next ) {
   if (!this.isModified('password')) return next();
   this.password = await bcrypt.hash(this.password, 10);
   next();
